8251ff35d24526864cfac2090104e09ab25f1b43
Sandy <70289658+sndyx@users.noreply.github.com>
2025-07-15 21:37:31 -0400

    Repl

--- ct_module/src/simulator/commands.ts
+++ ct_module/src/simulator/commands.ts
@@ -1,10 +1,16 @@
+import * as htsl from "htsl";
+
 import { Simulator } from "./simulator";
+import { printDiagnostic } from "../compiler/diagnostics";
 
 export function registerCommandTriggers(): CommandTrigger[] {
     return [
         register("command", 
             (...args) => commandFunction(args)
         ).setName("function"),
+        register("command",
+            (...args) => commandEval(args)
+        ).setName("/").setAliases("eval"),
     ];
 }
 
@@ -18,6 +24,23 @@ function commandFunction(args: string[]) {
             ChatLib.chat("&cInvalid usage: run <name>");
         }
         return;
+    }
+}
+
+function commandEval(args: string[]) {
+    console.log(args);
+    const source = args.join(" ").replace("\r", "");
+
+    const sm = new htsl.SourceMap();
+    sm.addFile(source, "source.htsl");
+
+    const result = htsl.parse.parseFromSourceMap(sm);
+    if (result.diagnostics.length !== 0) {
+        for (const diagnostic of result.diagnostics) {
+            printDiagnostic(sm, diagnostic);
+        }
+    } else {
+        Simulator.runActions(result.holders[0].actions!.value);
     }
 }
 
