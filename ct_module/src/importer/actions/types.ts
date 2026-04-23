c1ca75dccbdab0413952b65b6976978ca563eab1
Callanplays <163883917+Callanplays@users.noreply.github.com>
2026-04-22 21:33:15 -0500

    OBSERVED!!!

--- ct_module/src/importer/actions/types.ts
+++ ct_module/src/importer/actions/types.ts
@@ -1,4 +1,4 @@
-import type { Action } from "htsw/types";
+import type { Action, Condition } from "htsw/types";
 
 import type { ItemSlot } from "../../tasks/specifics/slots";
 
@@ -7,20 +7,33 @@ export type NestedListProp = "conditions" | "ifActions" | "elseActions" | "actio
 /** Nested list properties that still need to be read by clicking in. */
 export type NestedPropsToRead = Set<NestedListProp>;
 
-export type ObservedAction = {
+export type Observed<T> = {
+    [K in keyof T]: T[K] extends Action[]
+        ? Array<Observed<Action> | null>
+        : T[K] extends Condition[]
+          ? Array<Condition | null>
+          : T[K];
+};
+
+export type ObservedActionSlot = {
     index: number;
     slotId: number;
     slot: ItemSlot;
-    type: Action["type"];
-    action: Action;
-    propsToRead?: NestedPropsToRead;
+    action: Observed<Action> | null;
+};
+
+export type ObservedConditionSlot = {
+    index: number;
+    slotId: number;
+    slot: ItemSlot;
+    condition: Condition | null;
 };
 
 export type ActionListOperation =
-    | { kind: "move"; observed: ObservedAction; toIndex: number; action: Action }
-    | { kind: "edit"; observed: ObservedAction; desired: Action }
+    | { kind: "move"; observed: ObservedActionSlot; toIndex: number; action: Action }
+    | { kind: "edit"; observed: ObservedActionSlot; desired: Action }
     | { kind: "add"; desired: Action; toIndex: number }
-    | { kind: "delete"; observed: ObservedAction };
+    | { kind: "delete"; observed: ObservedActionSlot };
 
 export type ActionListDiff = {
     operations: ActionListOperation[];
