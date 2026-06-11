c2f4e03f342bc646cb2dd9b5c7416072507271e9
Sandy <70289658+sndyx@users.noreply.github.com>
2026-06-11 03:29:02 -0400

    Remove unused helpers file in nbt parsing

--- language/src/nbt/index.ts
+++ language/src/nbt/index.ts
@@ -1,5 +1,3 @@
 export * from "./parse";
 export * from "./print";
 export * from "./types";
-
-export * as helpers from "./parse/helpers";
--- language/src/nbt/parse/helpers.ts
+++
@@ -1,5 +0,0 @@
-import { Long } from "../../long";
-
-export function longToPair(value: Long): [number, number] {
-    return [value.high, value.low];
-}
