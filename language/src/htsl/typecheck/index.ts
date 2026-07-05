47664fcc9424d3e0baee1205102ea351dda6f4f2
Callanplays <163883917+Callanplays@users.noreply.github.com>
2026-07-05 15:09:49 -0700

    Delete unused files

--- language/src/htsl/typecheck/index.ts
+++
@@ -1,8 +0,0 @@
-import type { GlobalCtxt } from "../../context";
-import { check } from "./check";
-import { TyCtxt } from "./context";
-
-export function checkTypeflow(gcx: GlobalCtxt) {
-    const tcx = TyCtxt.fromGlobalCtxt(gcx);
-    check(tcx, []);
-}
